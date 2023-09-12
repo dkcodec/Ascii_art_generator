31a46e0f38a602b88b578e2ae355169e4b836ddb
DKod <74229630+dkcodec@users.noreply.github.com>
2023-09-12 18:17:44 +0600

    Update Asciigen.py

--- Asciigen.py
+++ Asciigen.py
@@ -9,41 +9,39 @@ charArray = list(chars)
 charsLen = len(charArray)
 interval = charsLen/256
 
-print(charsLen)
 
-text_file = open('output-text.txt', "w")
+scaleFactor = 0.17
+charHeight = 18
+charWidth = 8
+
 
 def getChar(InputInt):
     return charArray [math.floor(InputInt * interval)]
 
-# im = Image.open('Your photo')
+text_file = open('output-text.txt', "w")
 
-scaleFactor = 0.2
-charHeight = 18
-charWidth = 8
-width, height = im.size
+im = Image.open("Your photo in jpg")
 
 fnt = ImageFont.truetype('C:\\Windows\\Fonts\\lucon.ttf', 15)
 
-
-im = im.resize((int(scaleFactor*width*(charWidth/charHeight)), int(scaleFactor*height)), Image.NEAREST)
+width, height = im.size
+im = im.resize((int(scaleFactor*width), int(scaleFactor*height*(charWidth/charHeight))), Image.NEAREST)
 width, height = im.size
 pix = im.load()
 
-outputImage = Image.new( mode = 'RGB', size = (charWidth*height, charHeight*width), color = (10, 10, 10))
-
+outputImage = Image.new('RGB', (charWidth*width, charHeight*height), color = (0, 0, 0))
 d = ImageDraw.Draw(outputImage)
 
-for i in range(width):
-    for j in range(height):
-        r,g,b = pix[i,j]
+for i in range(height):
+    for j in range(width):
+        r, g, b = pix[j, i]
         h = int(r/3 + g/3 + b/3)
-        pix[i, j] = (h, h, h)
+        pix[j, i] = (h, h, h)
         text_file.write(getChar(h))
         d.text((j*charWidth, i*charHeight), getChar(h), font = fnt, fill = (r, g, b))
     text_file.write('\n')
 
 
-#outputImage.save('result.jpg')
+outputImage.save('result.jpg')
 
-#outputImage.show()
+outputImage.show()
